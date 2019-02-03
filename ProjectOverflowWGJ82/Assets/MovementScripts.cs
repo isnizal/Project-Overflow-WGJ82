9a1f41107fd339caa63008a3396e01a1ff1570ba
isnizal <31788317+isnizal@users.noreply.github.com>
2019-02-03 12:25:53 +0800

    animation walk complete

--- ProjectOverflowWGJ82/Assets/MovementScripts.cs
+++ ProjectOverflowWGJ82/Assets/MovementScripts.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovementScripts : MonoBehaviour {
-
+    public float speed = 10.0f;
+    private Animator playerAnimator;
 	// Use this for initialization
 	void Start () {
-		
+        playerAnimator = this.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float translation = Input.GetAxisRaw("Horizontal");
+        float translationUp = Input.GetAxisRaw("Vertical");
+        translation *= Time.deltaTime * speed;
+        translationUp *= Time.deltaTime * speed;
+
+        transform.Translate(translation, 0, 0);
+        transform.Translate(0, translationUp, 0);
+
 	}
 }
